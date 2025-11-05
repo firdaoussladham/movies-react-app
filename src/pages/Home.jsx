ffbdac9fca3f816d908a60f6bdcd9419add6c421
firdaoussladham <ladhamferdaous@gmail.com>
2025-11-06 00:22:51 +0100

    first-commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import '../css/Home.css'
+import { getPopularMovies, searchMovies } from "../services/api";
+
+function Home() {
+
+    const [searchQuery, setSearchQuery] = useState();
+    const [movies, setMovies]= useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+
+    useEffect(
+      ()=>{
+          const loadPopularMovies = async() =>{
+            try{
+                const popularMovies = await getPopularMovies();
+                setMovies(popularMovies)
+            }
+            catch(err){
+                console.log(err)
+                setError('Failed to load movies...')
+            }
+            finally{
+              setLoading(false)
+            }
+          }
+          loadPopularMovies();
+      }
+    ,[])
+
+  const handleSearch = async (e) =>{
+    e.preventDefault();
+    if(!searchQuery.trim()) return 
+    if(loading) return 
+
+    setLoading(true)
+    try{
+        const searchResults = await searchMovies(searchQuery)
+        setMovies(searchResults)
+        setError(null)
+    }catch(err){
+      console.log(err)
+      setError('Failed to search movies...')
+    }finally{
+      setLoading(false)
+    }
+  }
+  
+  return (
+    <div className="home">
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search movie ..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button className="search-button" type="submit">
+          Submit
+        </button>
+      </form>
+      <div className="movies-grid">
+        {movies.map ((movie) => (
+            <MovieCard movie={movie} key={movie.id}/>
+        ))}
+        </div>
+    </div>
+  );
+}
+
+export default Home;
